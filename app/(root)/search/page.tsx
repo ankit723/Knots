f27b7cf7df74b46f0eb7ca129dd127da31664860
Ankit Biswas <varanasiartist.omg@gmail.com>
2024-06-04 22:47:04 +0530

    search initiated

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,8 +1,51 @@
 
-const Page = async() => {
+import { currentUser } from "@clerk/nextjs/server"
+import {redirect} from 'next/navigation'
+import { fetchUser, fetchUsers } from "@/lib/actions/user.action";
+import ProfileHeader from "@/components/shared/profileHeader";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import KnotsTab from "@/components/shared/knotsTab";
+import UserCard from "@/components/cards/userCard";
+
+async function Page({params}:{params:{id:string}}){
+
+  const user=await currentUser()
+
+  if(!user) return null;
+
+  const result =await fetchUsers({
+    userId:user.id,
+    searchString:"",
+    pageNumber:1,
+    pageSize:25
+  })
+
+  console.log("all users are",result)
+
   return (
     <section>
         <h1 className="head-text mb-10">Search</h1>
+
+
+        <div className="mt-14 flex flex-col gap-9">
+          {result.users.length===0?(
+            <p className="no-result">No Users</p>
+          ):(
+            <>
+              {result.users.map((person)=>(
+                <UserCard 
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType="User"
+                />
+              ))}
+            </>
+          )}
+        </div>
     </section>
   )
 }
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -3,6 +3,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectedToDB } from "../mongoose";
 import Knot from "../models/knot.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
   userId: string;
@@ -79,5 +80,56 @@ export async function fetchUserPosts(userId:string){
     return knots;
   } catch (error:any) {
     throw new Error(`Failed to fetch Posts ${error.message}`)
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectedToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const regex = new RegExp(searchString, "i");
+
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId }, 
+    };
+
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
   }
 }
