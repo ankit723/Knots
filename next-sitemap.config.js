027f2843589e7ab940d6a94b00d357741d5b16db
Ankit Biswas <72332524+ankit723@users.noreply.github.com>
2025-01-09 12:31:36 +0530

    seo fixes initiated

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://knots.techmorphers.com',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
+  
