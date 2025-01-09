0f3b1bfcbc73a1cb622cf1239adfdb3a0cfaf08b
Ankit Biswas <72332524+ankit723@users.noreply.github.com>
2025-01-09 12:59:52 +0530

    seo fixes initiated 2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://knots.techmorphers.com',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
-  
+  siteUrl: 'https://knots.techmorphers.com',
+  generateRobotsTxt: true,
+  exclude: [],
+  sitemapSize: 7000,
+  transform: async (config, path) => {
+    console.log('Including in sitemap:', path);
+    return {
+      loc: path,
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    };
+  },
+};
